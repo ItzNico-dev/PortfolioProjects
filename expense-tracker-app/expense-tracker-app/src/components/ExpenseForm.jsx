8678d6a03617728c6df99d2b8928b7e2c3bd7a8c
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-09-24 20:30:31 +0300

    .

--- expense-tracker-app/expense-tracker-app/src/components/ExpenseForm.jsx
+++ expense-tracker-app/expense-tracker-app/src/components/ExpenseForm.jsx
@@ -17,28 +17,45 @@ const ExpenseForm = () => {
   };
 
   return (
-    <div className='expense-form'>
-      <h2>Add New Expense</h2>
+    <div className='expense-form p-4 bg-white rounded shadow'>
+      <h2 className='text-xl font-bold mb-4'>Add New Expense</h2>
       <form onSubmit={handleSubmit}>
-        <div className='form-control'>
-          <label htmlFor='text'>Text</label>
+        <div className='form-control mb-4'>
+          <label
+            htmlFor='text'
+            className='block text-gray-700 text-sm font-bold mb-2'
+          >
+            Text
+          </label>
           <input
             type='text'
             value={text}
             onChange={(e) => setText(e.target.value)}
+            className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
             required
           />
         </div>
-        <div className='form-control'>
-          <label htmlFor='amount'>Amount</label>
+        <div className='form-control mb-4'>
+          <label
+            htmlFor='amount'
+            className='block text-gray-700 text-sm font-bold mb-2'
+          >
+            Amount
+          </label>
           <input
             type='number'
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
+            className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline'
             required
           />
         </div>
-        <button type='submit'>Add Expense</button>
+        <button
+          type='submit'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+          Add Expense
+        </button>
       </form>
     </div>
   );
